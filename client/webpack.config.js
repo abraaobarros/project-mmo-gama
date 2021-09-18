032aa2a3439889a4340038bc0eb967c140593218
Abraao Lacerda <abraaobarros3@gmail.com>
2021-09-17 21:02:20 -0300

    Added webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+const { type } = require("os");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
